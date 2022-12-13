0466cd652e3771e07129e1aad32f1e39b4df9f71
ZHumay <114071477+ZHumay@users.noreply.github.com>
2022-12-13 00:40:12 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 import App from "./App";
 
-ReactDOM.render(
+ReactDOM.createRoot( document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
-  document.getElementById("root")
-);
+
+)
